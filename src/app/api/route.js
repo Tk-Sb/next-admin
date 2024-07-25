d1de9e2bfc1001e27eb166cd10d835adb4026482
Tk-Sb <pmn4222@gmail.com>
2024-07-25 11:26:57 +0300

    testing api

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,3 +1,5 @@
 export async function GET(){
-    return new Response("Hello world")
+    return new Response(JSON.stringify({ message: "Hello, World!" }), {  
+        headers: { "Content-Type": "application/json" },
+    })
 }
