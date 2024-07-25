d28dbbb556ae6a5bcdf1ab238854ce8252b77037
Tk-Sb <pmn4222@gmail.com>
2024-07-25 12:12:23 +0300

    adding the neon api key

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,10 +1,16 @@
 import { db } from "@/db/db"
 import { StudentsTable } from "@/db/schema"
 
-export async function GET(){
-    const data = await db.select().from(StudentsTable)
-
-    return new Response(JSON.stringify(data), {  
-        headers: { "Content-Type": "application/json" },
-    })
+export async function GET(request){
+    const apiKey = process.env.FETCH_API_KEY
+    const reqApiKey = request.headers.get('x-api-key')
+    if(apiKey === reqApiKey){
+        const data = await db.select().from(StudentsTable)
+        return new Response(JSON.stringify(data), {  
+            headers: { "Content-Type": "application/json" },
+        })
+    }
+    else{
+        return new Response(JSON.stringify({message: "failed"}))
+    }
 }
