2a18b1752aaf396842d21e7fed3713c94b78c44b
Tk-Sb <pmn4222@gmail.com>
2024-07-30 06:52:24 +0300

    cleaning out

--- src/app/student/page.jsx
+++ src/app/student/page.jsx
@@ -2,22 +2,21 @@ import { db } from "@/db/db"
 import { StudentsTable } from "@/db/schema"
 import { eq } from "drizzle-orm"
 import { cookies } from "next/headers"
-import { NextRequest } from "next/server"
 import { decrypt } from "../_lib/session"
 
 export default async function StudentProfile() {
+    // get the cookie stored in the client browser
     const cookie = cookies()
-    const session = cookie.get("session")
-    const decrypted = await decrypt(session.value)
-    console.log(decrypted.studentId)
-    // fetching data for student, returns { firstName: Tk, lastName: Sb, notes: [] }
+    const session = cookie.get("session") // returns the encrypted JWT value
+    const decrypted = await decrypt(session.value) // returns an object with the decrypted JWT value
+
+    // fetching data for student using his cookie, returns { firstName: Tk, lastName: Sb, notes: [] }
     const [studentData] = await db.select({
         firstName: StudentsTable.firstName,
         lastName: StudentsTable.lastName,
         notes: StudentsTable.notes
     }).from(StudentsTable).where(eq(StudentsTable.id, decrypted.studentId))
     
-    console.log(studentData)
     return(
         <>
             <h1>Profile for {studentData.firstName} {studentData.firstName}</h1>
